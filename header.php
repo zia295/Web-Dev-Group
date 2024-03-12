8a9d823f440d45be6d04da84c9b6f0582b4a3cb7
zia295 <74332450+zia295@users.noreply.github.com>
2024-03-12 15:10:48 +0000

    1

---
+++ header.php
@@ -0,0 +1,14 @@
+<?php
+$page_header1 = <<< HEADER
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+</head>
+<body>
+    
+</body>
+</html>
+
+HEADER;
