29cb1bffd63bff745867b484aadb6dcb8e5a5d06
Hassan Megahed <P2722142@LEC-ADMIN.dmu.ac.uk>
2024-04-15 13:48:19 +0100

    fix bug

--- create_device.php
+++ create_device.php
@@ -1,7 +1,6 @@
 <?php
 include 'header.php';
 
-
 // Check if the user is logged in
 if (!$isLoggedIn) {
     // Redirect the user to the login page if not logged in
@@ -25,24 +24,25 @@ echo $page_header;
 ?>
 
 <body>
-
-
-    <section class="add-device">
+    <section class="add-event">
         <h2>Add Device</h2>
-        <form class ="form-controll"  action = "createDeviceController.php" method="post" enctype="multipart/form-data">
+        <form class="form-controller" action="createDeviceController.php" method="post" enctype="multipart/form-data">
             <div class="form-group">
                 <label for="device_name">Device Name:</label>
-                <input type="text" id="device_name" name="device_name" required>
+                <input type="text" id="device_name" name="device_name" class="form-control" required>
             </div>
             <div class="form-group">
                 <label for="device_description">Description:</label>
-                <textarea id="device_description" name="device_description" required></textarea>
+                <textarea id="device_description" name="device_description" class="form-control" required></textarea>
             </div>
             <div class="form-group">
                 <label for="device_image">Image:</label>
-                <input type="file" id="device_image" name="device_image" required accept="image/*">
+                <input type="file" id="device_image" name="device_image" class="form-control" required accept="image/*">
             </div>
-            <button type="submit">Submit</button>
+            <div class="form-group">
+                <button type="submit" class="btn btn-primary">Submit</button>
+                <a href="manage_devices.php" class="btn btn-secondary">Cancel</a>
+            </div>
         </form>
     </section>
 
@@ -53,11 +53,12 @@ echo $page_header;
             Zia Hassankhail, Kader Zamoulli, Muaaz Patel, Hassan Mojahed, Hassan Choudhry
         </footer>
     </section>
-
     <script src="./script.js"></script>
+    </body>
+    </html>
+    FOOTER;
+
+    echo $page_footer;
+    ?>
 </body>
 </html>
-FOOTER;
-
-echo $page_footer;
-?>
